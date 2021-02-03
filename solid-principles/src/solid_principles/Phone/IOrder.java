c75d2ef99641c24d39128519148acb288e1a427f
25samyu <64177020+25samyu@users.noreply.github.com>
2021-02-03 12:42:29 +0530

    Delete solid-principles directory

--- solid-principles/src/solid_principles/Phone/IOrder.java
+++
@@ -1,6 +0,0 @@
-package solid_principles.Phone;
-
-public interface IOrder {
-	public String ProcessOrder(String modelName);
-
-}
--- solid-principles/src/solid_principles/Phone/IRepair.java
+++
@@ -1,8 +0,0 @@
-package solid_principles.Phone;
-
-public interface IRepair {
-	public String ProcessPhoneRepair(String modelName);
-
-	public String ProcessAccessoryRepair(String accessoryType);
-
-}
